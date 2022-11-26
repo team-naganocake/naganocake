1d8d582a016527b3b3f543140a2c22bc262ceec7
EC2 Default User <ec2-user@ip-172-31-44-194.ap-northeast-1.compute.internal>
2022-11-26 14:23:22 +0000

    11/26

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  #管理者のログアウト画面でルートエラーが出たため試しに記載してみた
+  config.sign_out_via = :get
 end

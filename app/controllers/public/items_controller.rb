603248df0bd34a3ddbc162d06a193285bc0785ab
EC2 Default User <ec2-user@ip-172-31-44-194.ap-northeast-1.compute.internal>
2022-12-04 16:12:14 +0000

    12/5

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,14 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @items = Item.all
+    @genres = Genre.all
+    if params[:genre_id].present?
+      #presentメソッドでparams[:genre_id]に値が含まれているか確認 => trueの場合下記を実行
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items
+      #@items = Item.where(genre: params[:genre_id])
+    end
   end
 
   def show

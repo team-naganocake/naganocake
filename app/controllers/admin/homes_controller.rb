634a1937401c0d9410e122e11535773ee8f61bd9
EC2 Default User <ec2-user@ip-172-31-44-194.ap-northeast-1.compute.internal>
2022-12-19 14:11:40 +0000

    12/19

---
+++ app/controllers/admin/homes_controller.rb
@@ -0,0 +1,10 @@
+class Admin::HomesController < ApplicationController
+
+before_action :authenticate_admin!
+#コントローラーに設定して、ログイン済管理者のみにアクセスを許可する。これ必要？
+
+  def top
+   @orders = Order.all.page(params[:page]).per(10).order('created_at DESC')
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    #【admin/homesコントローラ】
+    get '/' => 'homes#top'
+  end
+
+  namespace :admin do
     #【admin/genresコントローラー】
     resources :genres, only:[:index, :create, :edit, :update, :destroy]
   end
